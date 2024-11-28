afa3054985935385c344a6a98f5f2c64086b815c
Shivam Singh <shivamee0094@gmail.com>
2024-11-29 00:37:07 +0530

    128:Create save Post Feature Entity class added

---
+++ src/main/java/com/myblog/myblog12/entity/Post.java
@@ -0,0 +1,21 @@
+package com.myblog.myblog12.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Entity
+@Table(name = "posts")
+@Data//Here I am using lombok libraries to avoid boilerplate code and also getter and setter.
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String description;
+    private String content;
+}
